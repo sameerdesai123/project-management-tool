ea671442166ead54202be00c9c393e7c0b44de18
Sameer Desai <sameerdesai1008@gmail.com>
2020-05-24 11:35:43 +0530

    added task-view btns

--- components/TasksDisplay.vue
+++ components/TasksDisplay.vue
@@ -7,9 +7,20 @@
         v-for="(task,i) in allTasks"
         :key="i"
       >
-        <v-expansion-panel-header :class="task.status === strCmpltd ? success : task.status === strPrg ? info : danger">{{ task.taskTitle }}</v-expansion-panel-header>
+        <v-expansion-panel-header>{{ task.taskTitle }} &nbsp;
+          <span>
+            <v-chip :class="task.status === strCmpltd ? success : task.status === strPrg ? info : danger">
+              {{ task.status }}
+            </v-chip>
+            <v-btn :class="task.status === strCmpltd ? success : task.status === strPrg ? info : danger" x-small>
+            {{ btnFunc(task.status) }}
+            <span v-if="btnFunc(task.status) === 'Reopen'"><v-icon>mdi-plus</v-icon></span>
+            <span v-if="btnFunc(task.status) === 'Mark as Done'"><v-icon>mdi-checkbox-marked-circle-outline</v-icon></span>
+            <span v-if="btnFunc(task.status) === 'Begin'"><v-icon>mdi-near-me</v-icon></span>
+          </v-btn>
+          </span>
+        </v-expansion-panel-header>
         <v-expansion-panel-content>
-          Status : {{ task.status }} <br/>
           Description : {{ task.taskDescription }} <br/>
           Issues:
           <ol v-for="(issue, j) in task.issues" :key="j">
@@ -40,6 +51,13 @@
         danger: 'warning'
       }
     },
+    methods: {
+      btnFunc (status) {
+        if( status === 'Completed') return 'Reopen'
+        else if( status === 'In Progress') return 'Mark as Done'
+        else return 'Begin'
+      }
+    },
     computed: {
       todo () {
         let d = [];
@@ -67,5 +85,6 @@
 </script>
 
 <style lang="sass" scoped>
-
+.btnFunc
+  width: 50px  
 </style>
